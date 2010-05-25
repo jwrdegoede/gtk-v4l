b717a9e2160de025348a8efbc6ab956cbf118a40
Hans de Goede <hdegoede@redhat.com>
2010-05-25 21:14:15 +0200

    Make main control area its own GtkWidget

---
+++ src/gtk-v4l-widget.c
@@ -0,0 +1,359 @@
+/* gtk-v4l - GTK tool for control v4l camera properties
+ *
+ * Copyright (C) 2010 - Huzaifa Sidhpurwala <huzaifas@redhat.com>
+ * Copyright (C) 2010 - Hans de Goede <hdegoede@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *
+ */
+
+#include <linux/videodev.h>
+#include <libv4l2.h>
+#include "gtk-v4l-control.h"
+#include "gtk-v4l-widget.h"
+
+#define GTK_V4L_WIDGET_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), GTK_V4L_TYPE_WIDGET, Gtkv4lWidgetPrivate))
+
+enum
+{
+  PROP_0,
+  PROP_DEVICE,
+};
+
+struct _Gtkv4lWidgetPrivate {
+  gint placeholder;
+};
+
+/* will create gtk_v4l_widget_get_type and set gtk_v4l_widget_parent_class */
+G_DEFINE_TYPE (Gtkv4lWidget, gtk_v4l_widget, GTK_TYPE_TABLE);
+
+static void
+gtk_v4l_widget_set_property (GObject      *object,
+                             guint         property_id,
+                             const GValue *value,
+                             GParamSpec   *pspec)
+{
+  Gtkv4lWidget *self = GTK_V4L_WIDGET (object);
+
+  switch (property_id) {
+  case PROP_DEVICE:
+    self->device = g_value_dup_object (value);
+    break;
+  default:
+    /* We don't have any other property... */
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+static void
+gtk_v4l_widget_get_property (GObject    *object,
+                             guint       property_id,
+                             GValue     *value,
+                             GParamSpec *pspec)
+{
+  Gtkv4lWidget *self = GTK_V4L_WIDGET (object);
+
+  switch (property_id) {
+  case PROP_DEVICE:
+    g_value_set_object (value, self->device);
+    break;
+  default:
+    /* We don't have any other property... */
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+void int_control_changed_cb (GtkRange *range, gpointer user_data)
+{
+  Gtkv4lControl *control = GTK_V4L_CONTROL (user_data);
+  gdouble value;
+
+  value = gtk_range_get_value (GTK_RANGE (range));
+  gtk_v4l_control_set (control, value);
+  /* FIXME read back actual resulting value and update widget */
+}
+
+gchar *
+int_control_format_cb (GtkScale *scale, gdouble value, gpointer user_data)
+{
+  Gtkv4lControl *control = GTK_V4L_CONTROL (user_data);
+  gint div = control->maximum - control->minimum;
+
+  value -= control->minimum;
+
+  return g_strdup_printf("%5.2f %%", ((value / div) * 100) );
+}
+
+GtkWidget *v4l2_create_int_widget (Gtkv4lControl *control)
+{  
+  GtkWidget *HScale;
+  gint min, max, step,def;
+  min = control->minimum; max = control->maximum; step = control->step;
+
+  HScale = gtk_hscale_new_with_range (min,max,step);
+  gtk_scale_set_value_pos (GTK_SCALE(HScale), GTK_POS_RIGHT);
+  gtk_scale_set_digits (GTK_SCALE(HScale), 0);
+  gtk_range_set_increments (GTK_RANGE(HScale), step, step);
+  gtk_range_set_value (GTK_RANGE(HScale),
+                       (gdouble) gtk_v4l_control_get(control));
+  g_signal_connect (G_OBJECT (HScale), "value-changed", G_CALLBACK (int_control_changed_cb), control);
+  g_signal_connect (G_OBJECT (HScale), "format-value", G_CALLBACK (int_control_format_cb), control);
+
+  return HScale;
+}
+
+void bool_control_changed_cb (GtkButton *button, gpointer user_data)
+{
+  Gtkv4lControl *control = GTK_V4L_CONTROL (user_data);
+  gboolean state;
+
+  state = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON(button));
+  gtk_v4l_control_set (control, state);
+  /* FIXME read back actual resulting value and update widget */
+}
+
+GtkWidget *v4l2_create_bool_widget (Gtkv4lControl *control)
+{
+  GtkWidget *check;
+  gboolean state;
+
+  state = (gboolean) gtk_v4l_control_get (control);
+  check = gtk_check_button_new();
+  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(check), state);
+  g_signal_connect ( G_OBJECT(check), "clicked", G_CALLBACK (bool_control_changed_cb), control);
+
+  return check;
+}
+
+void menu_control_changed_cb (GtkWidget *wid, gpointer user_data)
+{
+  Gtkv4lControl *control = GTK_V4L_CONTROL (user_data);
+  gint value;
+
+  value = gtk_combo_box_get_active(GTK_COMBO_BOX(wid));
+  gtk_v4l_control_set (control, value);
+  /* FIXME read back actual resulting value and update widget */
+}
+
+/* FIXME HACK until menu enumeration is added to Gtkv4lControl */
+static int fd;
+
+GtkWidget *v4l2_create_menu_widget (Gtkv4lControl *control)
+{
+  GtkWidget *combo;
+  int k;
+
+  combo = gtk_combo_box_new_text();
+  for (k=control->minimum; k<=control->maximum; k++) {
+    struct v4l2_querymenu qm;
+          qm.id = control->id;
+          qm.index = k;
+          /* FIXME */
+    if(ioctl(fd, VIDIOC_QUERYMENU, &qm) == 0) 
+      gtk_combo_box_append_text (GTK_COMBO_BOX(combo), (const gchar *) qm.name);
+    else
+      g_warning ("Unable to use menu item for :%d", qm.index);
+  }
+
+  gtk_combo_box_set_active (GTK_COMBO_BOX(combo),
+                            gtk_v4l_control_get(control));
+  g_signal_connect (G_OBJECT(combo), "changed", G_CALLBACK(menu_control_changed_cb), control);
+
+  return combo;
+}
+
+void button_control_changed_cb (GtkButton *button, gpointer user_data)
+{
+  Gtkv4lControl *control = GTK_V4L_CONTROL (user_data);
+
+  gtk_v4l_control_set (control, 0xffffffff);
+}
+
+GtkWidget *v4l2_create_button_widget (Gtkv4lControl *control)
+{  
+  GtkWidget *button;
+  
+  button = gtk_button_new_with_label("Reset");
+  g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(button_control_changed_cb), control);
+
+  return button;
+}
+
+static GObject *
+gtk_v4l_widget_constructor (GType                  gtype,
+                            guint                  n_properties,
+                            GObjectConstructParam *properties)
+{
+  GObject *obj;
+  GList *elem;
+  Gtkv4lWidget *self;
+  GtkWidget *control_widget, *label, *align, *advanced_table = NULL;
+  struct v4l2_capability cap;
+  int rownum = 0, rownum_advanced = 0;
+
+  {
+    /* Always chain up to the parent constructor */
+    Gtkv4lWidgetClass *klass;
+    GObjectClass *parent_class;
+    parent_class = G_OBJECT_CLASS (gtk_v4l_widget_parent_class);
+    obj = parent_class->constructor (gtype, n_properties, properties);
+  }
+
+  /* update the object state depending on constructor properties */
+  self = GTK_V4L_WIDGET (obj);
+
+/* FIXME */
+fd = self->device->fd;
+
+  for (elem = g_list_first (gtk_v4l_device_get_controls (self->device));
+       elem; elem = g_list_next (elem)) {
+    Gtkv4lControl *control = GTK_V4L_CONTROL (elem->data);
+
+    switch(control->type) {
+    case V4L2_CTRL_TYPE_INTEGER:
+      control_widget = v4l2_create_int_widget (control);
+      break;
+    case V4L2_CTRL_TYPE_BOOLEAN:
+      control_widget = v4l2_create_bool_widget (control);
+      break;
+    case V4L2_CTRL_TYPE_MENU:
+      control_widget = v4l2_create_menu_widget (control);
+      break;
+    case V4L2_CTRL_TYPE_BUTTON:
+      control_widget = v4l2_create_button_widget (control);
+      break;
+    default:
+      g_warning("Skipping control %s with unknown type %d",
+                control->name, control->type);
+      continue;
+    }
+
+    control->user_data = control_widget;
+
+    gtk_widget_set_size_request (control_widget, 250, -1);
+
+    label = gtk_label_new ((gchar *)control->name);
+    gtk_misc_set_alignment (GTK_MISC(label),0.0,0.5);
+
+    align = gtk_alignment_new (0.0,0.5,0.0,0.0);
+    gtk_container_add (GTK_CONTAINER(align), control_widget);
+
+    /* FIXME */
+    if (FALSE) {
+       rownum_advanced++;
+       if (!advanced_table)
+         advanced_table = gtk_table_new(rownum_advanced, 2, FALSE);
+       else
+         gtk_table_resize (GTK_TABLE (advanced_table), rownum_advanced, 2);
+       gtk_table_attach (GTK_TABLE (advanced_table), label, 0,1, rownum_advanced, rownum_advanced + 1, GTK_FILL, GTK_FILL, 5, 5);
+       gtk_table_attach (GTK_TABLE (advanced_table), align, 1,2, rownum_advanced, rownum_advanced + 1, GTK_FILL, GTK_FILL, 5, 5);
+    } else {
+       rownum++;
+       gtk_table_resize (GTK_TABLE (self), rownum, 2);
+       gtk_table_attach (GTK_TABLE (self), label, 0,1, rownum, rownum + 1, GTK_FILL, GTK_FILL, 5, 5);
+       gtk_table_attach (GTK_TABLE (self), align, 1,2, rownum, rownum + 1, GTK_FILL, GTK_FILL, 5, 5);
+    }
+  }
+
+  if (advanced_table) {
+    GtkWidget *expander = gtk_expander_new ("More properties");
+
+    gtk_expander_set_expanded (GTK_EXPANDER (expander), FALSE);
+    gtk_container_add (GTK_CONTAINER (expander), advanced_table);
+    rownum++;
+    gtk_table_resize (GTK_TABLE(self), rownum, 2);
+    gtk_table_attach (GTK_TABLE(self), expander, 0, 2, rownum, rownum + 1, GTK_FILL, GTK_FILL, 0, 5);
+  }
+
+  return obj;
+}
+
+static void
+gtk_v4l_widget_finalize (GObject *object)
+{
+  Gtkv4lWidget *self = GTK_V4L_WIDGET (object);
+
+  g_object_unref (self->device);
+}
+
+static void
+gtk_v4l_widget_class_init (Gtkv4lWidgetClass *klass)
+{
+  GObjectClass *gobject_class = (GObjectClass *) klass;
+  GParamSpec *pspec;
+
+  g_type_class_add_private (klass, sizeof (Gtkv4lWidgetPrivate));
+
+  gobject_class->constructor  = gtk_v4l_widget_constructor;
+  gobject_class->finalize     = gtk_v4l_widget_finalize;
+  gobject_class->set_property = gtk_v4l_widget_set_property;
+  gobject_class->get_property = gtk_v4l_widget_get_property;
+
+  pspec = g_param_spec_object ("device",
+                               "Gtkv4lWidget construct prop",
+                               "Set the Gtkv4lDevice",
+                               GTK_V4L_TYPE_DEVICE,
+                               G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+  g_object_class_install_property (gobject_class,
+                                   PROP_DEVICE,
+                                   pspec);
+}
+
+static void
+gtk_v4l_widget_init (Gtkv4lWidget *self)
+{
+  const gchar *const subsystems[] = {"video4linux", NULL};
+  Gtkv4lWidgetPrivate *priv;
+
+  /* initialize the object */
+  self->priv = priv = GTK_V4L_WIDGET_GET_PRIVATE(self);
+}
+
+GtkWidget *
+gtk_v4l_widget_new (Gtkv4lDevice *device)
+{
+  return g_object_new (GTK_V4L_TYPE_WIDGET, "device", device, NULL);
+}
+
+void gtk_v4l_widget_reset_to_defaults (Gtkv4lWidget *self)
+{
+  GList *elem;
+
+  for (elem = g_list_first (gtk_v4l_device_get_controls (self->device));
+       elem; elem = g_list_next (elem)) {
+    Gtkv4lControl *control = GTK_V4L_CONTROL (elem->data);
+
+    switch (control->type) {
+    case V4L2_CTRL_TYPE_INTEGER:
+      gtk_range_set_value (GTK_RANGE (control->user_data),
+                           control->default_value);
+      break;
+    case V4L2_CTRL_TYPE_BOOLEAN:
+      gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (control->user_data),
+                                    control->default_value);
+      break;
+    case V4L2_CTRL_TYPE_MENU:
+      gtk_combo_box_set_active (GTK_COMBO_BOX (control->user_data),
+                                control->default_value);
+      break;
+    case V4L2_CTRL_TYPE_BUTTON:
+      /* do nothing */
+      break;
+    }
+  }
+}
--- src/gtk-v4l.c
+++ src/gtk-v4l.c
@@ -21,15 +21,17 @@
  */
 
 
-#include "gtk-v4l.h"
-#include "gtk-v4l-control.h"
+#include "gtk-v4l-widget.h"
 #include "gtk-v4l-device-list.h"
 
+#define ICON_LOC "/usr/share/icons/gnome/24x24/devices/camera-web.png"
+
 Gtkv4lDeviceList *devlist;
-Gtkv4lDevice *curr_dev;
+Gtkv4lDevice *curr_dev = NULL;
 GtkWidget *window,*advanced_window,*dev_combo;
 GtkTable *main_table,*table,*table2=NULL;
 GtkWidget *content_area,*content_area2;
+GtkWidget *controls;
 
 GtkWidget  *label_driver, *label_card, *label_bus;
 
@@ -42,169 +44,17 @@ GOptionEntry entries[] =
   };
 
 
-int rownum,rownum_advanced;
-
-void destroy(GtkWidget *widget, gpointer user_data)
+void close_cb (GtkWidget *w, gpointer user_data)
 {
 	gtk_main_quit();
 }
 
-void bool_control_changed_cb (GtkButton *button, gpointer user_data)
+void reset_cb (GtkButton *button, gpointer user_data)
 {
-	Gtkv4lControl *ctrl = GTK_V4L_CONTROL (user_data);
-	gboolean state;
-
-	state = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON(button));
-	gtk_v4l_control_set (ctrl, state);
-	/* FIXME read back actual resulting value and update widget */
+  Gtkv4lWidget *widget = GTK_V4L_WIDGET (user_data);
+  
+  gtk_v4l_widget_reset_to_defaults (widget);
 }
-
-void button_control_changed_cb (GtkButton *button, gpointer user_data)
-{
-	Gtkv4lControl *ctrl = GTK_V4L_CONTROL (user_data);
-
-	gtk_v4l_control_set (ctrl, 0xffffffff);
-}
-
-void int_control_changed_cb (GtkRange *range, gpointer user_data)
-{
-	Gtkv4lControl *ctrl = GTK_V4L_CONTROL (user_data);
-	gdouble value;
-
-	value = gtk_range_get_value (GTK_RANGE (range));
-	gtk_v4l_control_set (ctrl, value);
-	/* FIXME read back actual resulting value and update widget */
-}
-
-gchar *
-int_control_format_cb (GtkScale *scale, gdouble value, gpointer user_data)
-{
-	int max = GPOINTER_TO_INT(user_data);
-	return g_strdup_printf("%5.2f %%",  ((value/max)*100) );
-}
-
-void menu_control_changed_cb (GtkWidget *wid, gpointer user_data)
-{
-	Gtkv4lControl *ctrl = GTK_V4L_CONTROL (user_data);
-	gint value;
-
-	value = gtk_combo_box_get_active(GTK_COMBO_BOX(wid));
-	gtk_v4l_control_set (ctrl, value);
-	/* FIXME read back actual resulting value and update widget */
-}
-
-GtkWidget *v4l2_create_menu_widget (Gtkv4lControl *control)
-{
-	GtkWidget *combo;
-	int k;
-
-	combo = gtk_combo_box_new_text();
-	for (k=control->minimum; k<=control->maximum; k++) {
-		struct v4l2_querymenu qm;
-	        qm.id = control->id;
-	        qm.index = k;
-	        /* FIXME */
-		if(ioctl(curr_dev->fd, VIDIOC_QUERYMENU, &qm) == 0) 
-			gtk_combo_box_append_text (GTK_COMBO_BOX(combo), (const gchar *) qm.name);
-		else
-			g_warning ("Unable to use menu item for :%d", qm.index);
-	}
-
-	gtk_combo_box_set_active (GTK_COMBO_BOX(combo),
-	                          gtk_v4l_control_get(control));
-	g_signal_connect (G_OBJECT(combo), "changed", G_CALLBACK(menu_control_changed_cb), control);
-
-	return combo;
-}
-
-GtkWidget *v4l2_create_int_widget (Gtkv4lControl *control)
-{	
-	GtkWidget *HScale;
-	gint min, max, step,def;
-	min = control->minimum; max = control->maximum; step = control->step;
-
-	HScale = gtk_hscale_new_with_range (min,max,step);
-	gtk_scale_set_value_pos (GTK_SCALE(HScale), GTK_POS_RIGHT);
-	gtk_scale_set_digits (GTK_SCALE(HScale), 0);
-	gtk_range_set_increments (GTK_RANGE(HScale), step, step);
-	gtk_range_set_value (GTK_RANGE(HScale),
-	                     (gdouble) gtk_v4l_control_get(control));
-	g_signal_connect (G_OBJECT (HScale), "value-changed", G_CALLBACK (int_control_changed_cb), control);
-	g_signal_connect (G_OBJECT (HScale), "format-value", G_CALLBACK (int_control_format_cb), GINT_TO_POINTER(max));
-
-	return HScale;
-}
-
-GtkWidget *v4l2_create_button_widget (Gtkv4lControl *control)
-{	
-	GtkWidget *button;
-	
-	button = gtk_button_new_with_label("Reset");
-	g_signal_connect(G_OBJECT(button),"clicked",G_CALLBACK(button_control_changed_cb), control);
-
-	return button;
-}
-
-GtkWidget *v4l2_create_bool_widget (Gtkv4lControl *control)
-{
-	GtkWidget *check;
-	gboolean state;
-
-	state = (gboolean) gtk_v4l_control_get (control);
-	check = gtk_check_button_new();
-	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(check), state);
-	g_signal_connect ( G_OBJECT(check), "clicked", G_CALLBACK (bool_control_changed_cb), control);
-
-	return check;
-}
-
-void v4l2_add_control (Gtkv4lControl *control)
-{
-	GtkWidget *widget, *label, *align;
-
-	switch(control->type) {
-		case V4L2_CTRL_TYPE_INTEGER:
-			widget = v4l2_create_int_widget (control);
-			break;
-		case V4L2_CTRL_TYPE_BOOLEAN:
-			widget = v4l2_create_bool_widget (control);
-			break;
-		case V4L2_CTRL_TYPE_MENU:
-			widget = v4l2_create_menu_widget (control);
-			break;
-		case V4L2_CTRL_TYPE_BUTTON:
-			widget = v4l2_create_button_widget (control);
-			break;
-		default:
-			g_warning("Skipping control %s with unknown type %d",
-			          control->name, control->type);
-			return;
-	}
-
-	control->user_data = widget;
-
-        gtk_widget_set_size_request (widget, 250, -1);
-
-	label = gtk_label_new ((gchar *)control->name);
-	gtk_misc_set_alignment (GTK_MISC(label),0.0,0.5);
-
-	align = gtk_alignment_new (0.0,0.5,0.0,0.0);
-	gtk_container_add (GTK_CONTAINER(align), widget);
-
-        /* FIXME */
-        if (FALSE) {
-                rownum_advanced++;
-                gtk_table_resize (table2, rownum_advanced, 2);
-                gtk_table_attach (GTK_TABLE(table2), label, 0,1,rownum_advanced, rownum_advanced+1, GTK_FILL, GTK_FILL, 5, 5);
-                gtk_table_attach (GTK_TABLE(table2), align, 1,2,rownum_advanced, rownum_advanced+1, GTK_FILL, GTK_FILL, 5, 5);
-        } else {
-                rownum++;
-                gtk_table_resize (table, rownum, 2);
-                gtk_table_attach (GTK_TABLE(table), label, 0,1,rownum, rownum+1, GTK_FILL, GTK_FILL, 5, 5);
-                gtk_table_attach (GTK_TABLE(table), align, 1,2,rownum, rownum+1, GTK_FILL, GTK_FILL, 5, 5);
-        }
-}
-
 
 void v4l2_add_header_init ()
 {
@@ -269,95 +119,16 @@ v4l2_add_header_populate(Gtkv4lDevice *device)
 	gtk_label_set_text(GTK_LABEL(label_bus), device->bus_info);
 }
 
-void v4l2_reset_defaults(void)
-{
-  GList *elem;
-
-  for (elem = g_list_first (gtk_v4l_device_get_controls (curr_dev));
-       elem; elem = g_list_next (elem)) {
-    Gtkv4lControl *control = GTK_V4L_CONTROL (elem->data);
-
-    switch (control->type) {
-    case V4L2_CTRL_TYPE_INTEGER:
-      gtk_range_set_value (GTK_RANGE (control->user_data),
-                           control->default_value);
-      break;
-    case V4L2_CTRL_TYPE_BOOLEAN:
-      gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (control->user_data),
-                                    control->default_value);
-      break;
-    case V4L2_CTRL_TYPE_MENU:
-      gtk_combo_box_set_active (GTK_COMBO_BOX (control->user_data),
-                                control->default_value);
-      break;
-    case V4L2_CTRL_TYPE_BUTTON:
-      /* do nothing */
-      break;
-    }
-  }
-}
-
-void v4l2_load_from_driver (void)
-{
-  GList *elem;
-
-  for (elem = g_list_first (gtk_v4l_device_get_controls (curr_dev));
-       elem; elem = g_list_next (elem)) {
-    Gtkv4lControl *control = GTK_V4L_CONTROL (elem->data);
-
-    v4l2_add_control (control);
-  }
-}
-
-void v4l2_control_panel_create_properties(void)
-{
-	advanced_window = gtk_expander_new("More properties");
-
-	gtk_expander_set_expanded(GTK_EXPANDER(advanced_window), FALSE);
-
-	table = GTK_TABLE(gtk_table_new(1,3,FALSE));
-
-        /* FIXME */
-	if (FALSE) {
-		table2 = GTK_TABLE(gtk_table_new(rownum_advanced, 3, FALSE));
-		gtk_container_add (GTK_CONTAINER(advanced_window), GTK_WIDGET(table2));
-
-		v4l2_load_from_driver ();
-		v4l2_add_footer (TRUE);
-
-	} else {
-		v4l2_load_from_driver ();
-		v4l2_add_footer (FALSE);			
-		}
-
-        gtk_table_attach (GTK_TABLE(main_table),GTK_WIDGET(table), 0,2,rownum, rownum+1,GTK_FILL, GTK_FILL, 0, 5);
-}
-
 void v4l2_control_panel_create (Gtkv4lDevice *device)
 {
-	rownum=5;
-	main_table = GTK_TABLE(gtk_table_new (rownum,3,FALSE));
+	main_table = GTK_TABLE(gtk_table_new (5, 2, FALSE));
 	v4l2_add_header_init();
 	v4l2_add_header_populate(device);
 
-	v4l2_control_panel_create_properties();
+        controls = gtk_v4l_widget_new (device);
+        gtk_table_attach (GTK_TABLE(main_table), controls, 0, 2, 5, 6, GTK_FILL, GTK_FILL, 0, 5);
 	
 	gtk_container_add (GTK_CONTAINER(content_area),GTK_WIDGET(main_table));
-
-}
-
-void v4l2_add_footer (gboolean advanced)
-{
-        rownum++;
-	gtk_table_resize (table, rownum, 2);
-
-        gtk_table_attach (GTK_TABLE(table),advanced_window, 0,2,rownum, rownum+1,GTK_FILL, GTK_FILL, 0, 5);
-	gtk_widget_set_sensitive(GTK_WIDGET(advanced_window),advanced);
-
-
-	rownum++;
-        gtk_table_resize (table, rownum, 2);
-
 }
 
 void v4l2_add_dialog_buttons(void)
@@ -365,11 +136,10 @@ void v4l2_add_dialog_buttons(void)
 	GtkWidget *button;
 
         button = gtk_dialog_add_button ( GTK_DIALOG (window), "_Defaults", GTK_RESPONSE_APPLY);
-        g_signal_connect(G_OBJECT(button),"clicked", G_CALLBACK(v4l2_reset_defaults),NULL);
+        g_signal_connect(G_OBJECT(button),"clicked", G_CALLBACK(reset_cb), controls);
 
         button = gtk_dialog_add_button ( GTK_DIALOG (window), GTK_STOCK_CLOSE, GTK_RESPONSE_CLOSE);
-        g_signal_connect(G_OBJECT(button),"clicked", G_CALLBACK(destroy), NULL);
-
+        g_signal_connect(G_OBJECT(button),"clicked", G_CALLBACK(close_cb), NULL);
 }
 
 void
@@ -391,9 +161,10 @@ void v4l2_switch_to_new_device(Gtkv4lDevice *device)
 {
 	curr_dev = device;
 	v4l2_add_header_populate(device);
-	gtk_widget_destroy(GTK_WIDGET(table));
-	v4l2_control_panel_create_properties();
-	gtk_widget_show_all(GTK_WIDGET(table));
+	gtk_widget_destroy(controls);
+        controls = gtk_v4l_widget_new (device);
+        gtk_table_attach (GTK_TABLE(main_table), controls, 0, 2, 5, 6, GTK_FILL, GTK_FILL, 0, 5);
+	gtk_widget_show_all(controls);
 }
 
 void v4l2_combo_change_device_cb (GtkWidget *wid, gpointer user_data)
@@ -436,18 +207,14 @@ int main(int argc, char *argv[])
 
   gtk_init (&argc, &argv);
 
-
-  if (device == NULL) // Assume default device
+  if (device)
   {
-	curr_dev = g_list_nth_data(devlist->list, 0);
+    curr_dev = gtk_v4l_device_list_get_dev_by_device_file (devlist, device);
+    if (!curr_dev)
+      show_error_dialog ("Specified V4L2 device not found");
   }
-  else
-        curr_dev = gtk_v4l_device_list_get_dev_by_device_file(devlist, device);
 
-  if (!curr_dev) {
-          show_error_dialog ("No V4L2 devices found");
-          return EXIT_FAILURE;
-  }
+  curr_dev = g_list_nth_data(devlist->list, 0);
 
   window = gtk_dialog_new();
   gtk_window_set_title(GTK_WINDOW(window), "GTK V4L Device properties");
@@ -464,11 +231,10 @@ int main(int argc, char *argv[])
   
   gtk_container_set_border_width (GTK_CONTAINER(window),7);
 
-
   v4l2_control_panel_create(curr_dev);
 
   v4l2_add_dialog_buttons();
-  g_signal_connect(G_OBJECT(window),"destroy", G_CALLBACK(destroy), NULL);
+  g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(close_cb), NULL);
 
   gtk_widget_show_all (window);
 
