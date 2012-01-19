8fb77c5ba3847c64c9e556b957b225e13fd1f773
Huzaifa Sidhpurwala <huzaifas@redhat.com>
2012-01-19 16:49:08 +0530

    Split into library and binary

--- src/gtk-v4l.c
+++ src/gtk-v4l.c
@@ -18,8 +18,8 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "gtk-v4l-widget.h"
-#include "gtk-v4l-device-list.h"
+#include <gtk-v4l-widget.h>
+#include <gtk-v4l-device-list.h>
 
 #define ICON_LOC "/usr/share/icons/gnome/24x24/devices/camera-web.png"
 
